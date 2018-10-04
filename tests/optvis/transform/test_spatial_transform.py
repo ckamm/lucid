tests: fix weighted average for dot location
For projections like shears the values won't sum to one.

tests/optvis/transform/test_spatial_transform.py
@@ -26,7 +26,7 @@ def _find_single_dot(array):
     assert np.all(value_range < MAX_SPREAD)
 
     values = array[indices]
-    weighted_location = values.dot(np.transpose(indices))
+    weighted_location = values.dot(np.transpose(indices)) / sum(values)
     return weighted_location
 
 MAX_SPREAD = 3 # px that a single pixel may have been stretched over