transform: fix computation of linear transform
Trying to decompose a linear transforms as
  rotate(a) * rotate(-b) * (1, x; y, 1) * rotate(b)
does not work, one degree of freedom is missing: By QR decomposition
with Q=rotate(a) * rotate(-b) and R=(1, x; y, 1) * rotate(b) we'd need
to be able to make an arbitrary upper triangular R. But if we create the
zero only two degrees of freedom are left - not enough.

Due to SVD it's clear that
  rotate(a) * rotate(-b) * (x, 0; 0, y) * rotate(b) can represent all
linear transformations.

lucid/optvis/transform/operators.py
@@ -32,20 +32,15 @@ def _rotation(angleInRadians, axis):
     else:
         raise ValueError("Axis needs to be one of (0,1,2)!")
 
-
-def _deformation(x, y):
-    matrix = np.identity(3, dtype=np.float64)
-    matrix[0, 1] = x
-    matrix[1, 0] = y
-    return matrix
-
-
-def _shearing(angle, x, y):
+def _scaling(angle, x, y):
     axis = 2 # z
     rotate = _rotation(angle, axis)
     unrotate = _rotation(-angle, axis)
-    deform = _deformation(x, y)
-    return  np.matmul(unrotate, np.matmul(deform, rotate))
+
+    basescale = np.identity(3, dtype=np.float64)
+    basescale[0, 0] = x
+    basescale[1, 1] = y
+    return  np.matmul(unrotate, np.matmul(basescale, rotate))
 
 
 def _translation(translation_x, translation_y):
@@ -63,7 +58,7 @@ def _projection(vanishing_point_1, vanishing_point_2):
 
 
 def _homography(
-    initial_translation, rotation, shear, projection, final_translation, shape
+    initial_translation, rotation, scaling, projection, final_translation, shape
 ):
     center = _translation(shape[0] // 2, shape[1] // 2)
     uncenter = _translation(-shape[0] // 2, -shape[1] // 2)
@@ -72,8 +67,8 @@ def _homography(
         np.matmul(final_translation,
         np.matmul(center,
         np.matmul(projection,
-        np.matmul(shear,
         np.matmul(rotation,
+        np.matmul(scaling,
         np.matmul(uncenter,
                   initial_translation))))))
     )
@@ -83,9 +78,9 @@ def _parameterized_flattened_homography(
     translation1_x,
     translation1_y,
     rotationAngleInRadians,
-    shearingAngleInRadians,
-    shear_x,
-    shear_y,
+    scalingAngleInRadians,
+    scaling_x,
+    scaling_y,
     vanishing_point_x,
     vanishing_point_y,
     translation2_x,
@@ -95,11 +90,11 @@ def _parameterized_flattened_homography(
     initial_translate = _translation(translation1_x, translation1_y)
     axis = 2  # z-axis: rotate a 2D image in its plane
     rotate = _rotation(rotationAngleInRadians, axis)
-    shear = _shearing(shearingAngleInRadians, shear_x, shear_y)
+    scale = _scaling(scalingAngleInRadians, scaling_x, scaling_y)
     project = _projection(vanishing_point_x, vanishing_point_y)
     final_translate = _translation(translation2_x, translation2_y)
     matrix = _homography(
-        initial_translate, rotate, shear, project, final_translate, shape_xy
+        initial_translate, rotate, scale, project, final_translate, shape_xy
     )
     #
     # conform to tf.contrib.image.transform interface

lucid/optvis/transform/spatial.py
@@ -106,7 +106,7 @@ def rotate(angles, units="degrees", interpolation="BILINEAR", seed=None):
 
     return inner
 
-def default_homography_parameters(shape, seed=None):
+def homography_parameters_random(shape, seed=None):
     """Returns parameters for homography() that create a random transform
     that usually results in only slight adjustment of the image"""
     d = tf.reduce_max(shape)
@@ -114,9 +114,9 @@ def default_homography_parameters(shape, seed=None):
       translation1_x = tf.truncated_normal([], stddev=3, seed=seed),
       translation1_y = tf.truncated_normal([], stddev=3, seed=seed),
       rotationAngleInRadians = angle2rads(tf.truncated_normal([], stddev=2.5, seed=seed)),
-      shearingAngleInRadians = angle2rads(tf.truncated_normal([], stddev=2.5, seed=seed)),
-      shear_x = tf.truncated_normal([], stddev=1e-2, seed=seed),
-      shear_y = tf.truncated_normal([], stddev=1e-2, seed=seed),
+      scalingAngleInRadians = angle2rads(tf.truncated_normal([], stddev=2.5, seed=seed)),
+      scaling_x = 1.0 + tf.truncated_normal([], stddev=1e-2, seed=seed, dtype=tf.float64),
+      scaling_y = 1.0 + tf.truncated_normal([], stddev=1e-2, seed=seed, dtype=tf.float64),
 
       # Intuition for vanishing_points:
       # - values above roughly 1/(size/2) make no sense because one edge of the
@@ -130,15 +130,15 @@ def default_homography_parameters(shape, seed=None):
       translation2_y = tf.truncated_normal([], stddev=2, seed=seed),
     )
 
-def null_homography_parameters():
+def homography_parameters_identity():
     """Returns parameters for homography() that make it an identity-transform"""
     return dict(
       translation1_x = 0.0,
       translation1_y = 0.0,
       rotationAngleInRadians = 0.0,
-      shearingAngleInRadians = 0.0,
-      shear_x = 0.0,
-      shear_y = 0.0,
+      scalingAngleInRadians = 0.0,
+      scaling_x = 1.0,
+      scaling_y = 1.0,
       vanishing_point_x = 0.0,
       vanishing_point_y = 0.0,
       translation2_x = 0.0,
@@ -150,13 +150,13 @@ def homography(parameters=None, seed=None, interpolation="BILINEAR"):
     Consists of an affine transformation + a perspective projection.
 
     By default, when parameters is None, a random homography based on
-    default_homography_parameters() is performed.
+    homography_parameters_random() is performed.
     """
 
     def inner(image_t):
         shape_xy = tf.shape(image_t)[1:3]
         if parameters is None:
-            params = default_homography_parameters(shape_xy, seed)
+            params = homography_parameters_random(shape_xy, seed)
         else:
             params = parameters
 
@@ -166,9 +166,9 @@ def homography(parameters=None, seed=None, interpolation="BILINEAR"):
                 params['translation1_x'],
                 params['translation1_y'],
                 params['rotationAngleInRadians'],
-                params['shearingAngleInRadians'],
-                params['shear_x'],
-                params['shear_y'],
+                params['scalingAngleInRadians'],
+                params['scaling_x'],
+                params['scaling_y'],
                 params['vanishing_point_x'],
                 params['vanishing_point_y'],
                 params['translation2_x'],

tests/optvis/transform/test_spatial_transform.py
@@ -10,7 +10,7 @@ from lucid.optvis.transform.spatial import (
     jitter,
     pad,
     homography,
-    null_homography_parameters,
+    homography_parameters_identity,
     crop_or_pad_to_shape,
 )
 # from lucid.misc.io import save
@@ -64,17 +64,17 @@ def test_spatial(transform, arg):
   ({'rotationAngleInRadians': math.pi / 2}, (128 + 32, 64 + 16), (128 - 16, 64 + 32)),
   ({'rotationAngleInRadians': math.pi / 2, 'translation1_x': 3}, (128 + 32, 64 + 16), (128 - 16, 64 + 32 + 3)), # effectively move rotation center 3 to the left
   ({'rotationAngleInRadians': math.pi / 2, 'translation2_x': 3}, (128 + 32, 64 + 16), (128 - 16 + 3, 64 + 32)),
-  ({'shearingAngleInRadians': 3.0}, (160, 80), (160, 80)),
-  ({'shear_x': 0.5}, (160, 80), (128 + 32 + 16 / 2, 80)),
-  ({'shear_y': 0.5}, (160, 80), (160, 64 + 16 + 32 / 2)),
-  ({'shearingAngleInRadians': math.pi / 2, 'shear_x': -0.5}, (160, 80), (160, 64 + 16 + 32 / 2)),
+  ({'scalingAngleInRadians': 3.0}, (160, 80), (160, 80)),
+  ({'scaling_x': 0.5}, (160, 80), (128 + 32 / 2, 80)),
+  ({'scaling_y': 0.5}, (160, 80), (160, 64 + 16 / 2)),
+  ({'scalingAngleInRadians': math.pi / 2, 'scaling_x': 0.5}, (160, 80), (160, 64 + 16 / 2)),
   ({'vanishing_point_x': 1 / 128}, (128, 80), (128, 80)),
   ({'vanishing_point_x': 1 / 128}, (160, 80), (128 + 32 / (1 + 32 / 128), 64 + 16 / (1 + 32 / 128))),
   ({'vanishing_point_y': 1 / 128}, (160, 64), (160, 64)),
   ({'vanishing_point_y': 1 / 128}, (160, 80), (128 + 32 / (1 + 16 / 128), 64 + 16 / (1 + 16 / 128))),
 ])
 def test_homography(params, start, expected):
-    params = dict(null_homography_parameters(), **params)
+    params = dict(homography_parameters_identity(), **params)
     image = np.zeros([1, 256, 128, 1])
     image[0, start[0], start[1]] = np.ones(())
     with tf.Session() as sess: